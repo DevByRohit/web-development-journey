d90ab374adf401fa8a35083ffee9fb2edb52a8bf
Rohit Ahirwar <work.rohitahirwar@gmail.com>
2024-08-25 11:48:12 +0530

    learnt about DOM & BOM, Events, and localStorage and built some projects releted to these concepts

---
+++ C-JavaScript/Day_13/localStorageBrowser.js
@@ -0,0 +1,8 @@
+// Local Storage
+
+localStorage.setItem("key", "value");
+let retrievedValue = localStorage.getItem("key");
+localStorage.removeItem("key");
+
+// todo localStorage can only store the string data if you want to store complex data structure like array, object etc. you need to convert it into a string using JSON.stringify.
+
